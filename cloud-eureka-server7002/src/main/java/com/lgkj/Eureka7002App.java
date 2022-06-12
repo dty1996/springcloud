6772c453dcdd814ecceb76de0d39bff129093ff8
dty <dantangyao@hblg.vip>
2022-06-12 16:46:23 +0800

    eureka集群

--- cloud-eureka-server7002/src/main/java/com/lgkj/Eureka7002App.java
+++ cloud-eureka-server7002/src/main/java/com/lgkj/Eureka7002App.java
@@ -1,2 +1,16 @@
-package com.lgkj;public class Eureka7002App {
+package com.lgkj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author dty
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka7002App {
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka7002App.class, args);
+    }
 }
