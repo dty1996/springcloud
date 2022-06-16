1e94da68100f476bb2e8fced95bebe03df80f5c7
dty <dantangyao@hblg.vip>
2022-06-16 21:33:02 +0800

    springCloud gateway

---
+++ cloud-gateway-gateway9527/src/main/java/com/lgkj/Gateway9527App.java
@@ -0,0 +1,14 @@
+package com.lgkj;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author dty
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class Gateway9527App {
+
+}
