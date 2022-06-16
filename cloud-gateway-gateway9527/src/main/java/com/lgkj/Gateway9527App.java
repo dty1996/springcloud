f5b0a2d765dd2a28aba916ec3b2816b772bedd37
dty1996 <1451069487@qq.com>
2022-06-16 23:56:13 +0800

    Gateway服务网关

--- cloud-gateway-gateway9527/src/main/java/com/lgkj/Gateway9527App.java
+++ cloud-gateway-gateway9527/src/main/java/com/lgkj/Gateway9527App.java
@@ -1,6 +1,7 @@
 package com.lgkj;
 
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
@@ -10,5 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableEurekaClient
 public class Gateway9527App {
-
+    public static void main(String[] args) {
+        SpringApplication.run(Gateway9527App.class, args);
+    }
 }
