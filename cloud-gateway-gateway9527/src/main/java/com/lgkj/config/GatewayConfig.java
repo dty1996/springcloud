1119b9541fc04f13e4ebf438aed74535ddda06dc
dty <dantangyao@hblg.vip>
2022-06-17 18:01:08 +0800

    springCloud gateway 设置

--- cloud-gateway-gateway9527/src/main/java/com/lgkj/config/GatewayConfig.java
+++ cloud-gateway-gateway9527/src/main/java/com/lgkj/config/GatewayConfig.java
@@ -6,12 +6,27 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * @author dty
+ */
 @Configuration
 public class GatewayConfig {
+
+    /**
+     * 路由匹配
+     * lb为负载均衡
+     * @param builder
+     * @return
+     */
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder){
-        RouteLocatorBuilder.Builder routes = builder.routes();
-        routes.route("payment", r -> r.path("/guonei").uri("https://news.baidu.com/guonei")).build();
-        return routes.build();
+        return  builder
+                .routes()
+                .route("payment",
+                        r -> r.path("/guonei").uri("https://news.baidu.com"))
+                .route("payment_lb",
+                        r -> r.path("/payment/get/**").uri("lb://PaymentApp"))
+                .build();
+
     }
 }
