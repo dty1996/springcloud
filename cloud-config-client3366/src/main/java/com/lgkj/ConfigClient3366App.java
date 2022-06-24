6507dc1d26822a479036906fc5fe7330f1ce879c
dty1996 <1451069487@qq.com>
2022-06-24 17:30:16 +0800

    spring bus 服务总线设置

--- cloud-config-client3366/src/main/java/com/lgkj/ConfigClient3366App.java
+++ cloud-config-client3366/src/main/java/com/lgkj/ConfigClient3366App.java
@@ -2,17 +2,15 @@ package com.lgkj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author dty
  */
 @SpringBootApplication
-@EnableConfigServer
 @EnableEurekaClient
-public class ConfigCenter3344App {
+public class ConfigClient3366App {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigCenter3344App.class, args);
+        SpringApplication.run(ConfigClient3366App.class, args);
     }
 }
